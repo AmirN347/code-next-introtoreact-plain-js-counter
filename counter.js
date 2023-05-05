c0208a10278c2c8fd8b43db692a989e28abf8737
AmirN347 <108190581+AmirN347@users.noreply.github.com>
2023-05-05 16:26:49 -0400

    added new features for counter.js

--- counter.js
+++ counter.js
@@ -4,12 +4,21 @@ const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 
 var counter = 0;
+var incrementButtonCount = 0;
 incrementButton.addEventListener('click', () => {
   count.innerHTML = counter++;
+  incrementButtonCount++;
+  if (incrementButtonCount > 10) {
+    console.log('You have clicked too many times');
+  } else {
+    console.log('Incremented button');
+  }
+
+  console.log({ counter });
 });
 
 decrementButton.addEventListener('click', () => {
-  
+  //if statement using syntatic sugar condition? true excuetion: false excuetion
 
   if (counter - 1 < 0) {
     alert('cant go below 0');
@@ -17,4 +26,7 @@ decrementButton.addEventListener('click', () => {
     counter--;
     count.innerHTML = counter;
   }
+
+  console.log('Decremented button');
+  console.log({ counter });
 });
